82eb22406184fcfc61b64af34e1977c70930ddb4
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-02-04 09:22:35 +0100

    adding some change to js

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,18 +1,19 @@
 
-const btns = document.querySelectorAll('.question');
-btns.forEach(btn => {
-  btn.addEventListener('click', (e)=>{
-    
-  if(e.currentTarget.nextElementSibling.classList.contains = "hide"){
-      e.currentTarget.nextElementSibling.classList.toggle('show');
-      e.currentTarget.classList.toggle('bold');
-      const img = e.currentTarget.querySelector('img');
-      img.classList.toggle('rotate')
-     }
-    
-  });
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(el => {
+    const btn = el.parentElement;
+    btn.addEventListener('click', () => {
+        questions.forEach(item => {
+            const perent = item.parentElement;
+            if (perent !== btn) {
+                perent.classList.remove('show')
+                item.classList.remove('rotate');
+
+            }
+        });
+        btn.classList.toggle('show')
+        el.classList.toggle('rotate');
+        
+    })
 });
-
-
-
-
