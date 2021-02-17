c39fdb2ce89212d497e6b3f0bc544762acc3ce8e
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-02-17 13:52:09 +0100

    style css and butons js

---
+++ coding-bootcamp-testimonials-slider-master/script.js
@@ -0,0 +1,41 @@
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+
+slides.forEach((slide, index) => {
+    slide.style.position= 'absolute';
+    slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click', ()=>{
+    counter++
+    carousel();
+    console.log(nextBtn);
+
+});
+
+prevBtn.addEventListener('click', ()=>{
+    counter--
+    carousel();
+    console.log(prevBtn);
+});
+
+
+function carousel(){
+    if(counter === slides.length){
+        counter = 0
+    }
+    if(counter < 0){
+    counter = slides.length-1;
+    }
+
+    slides.forEach((slide)=>{
+    slide.style.transform = `translateX(-${counter * 100}%)`
+    
+});
+
+};
+
+    
