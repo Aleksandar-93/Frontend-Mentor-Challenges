1720fd58d5dc36f841dd40afc07e22969ab1393c
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-02-27 22:38:13 +0100

    validate email

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,22 +1,45 @@
 const btn = document.getElementById('free-trail');
 const input = document.querySelectorAll('.input-wrap input')
+const email = document.getElementById('mail');
 const inputWrap = document.querySelectorAll('.input-wrap');
 
+// Check if input is empty display error
 btn.addEventListener('click', ifEmptyCheck);
 
 function ifEmptyCheck (){
-
     input.forEach(element => {
         if(element.value == ""){
             element.parentElement.classList.add("error");
-        
         } else{
             element.parentElement.classList.remove("error");
-            
         }
+    });
+}
+
+   // check if email is valid     
+    
+  
+     // validate Email
+   function validateEmail(email) {
+       const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+       return re.test(email);
+     }
+   
+     function validate() {
+       const emValue = email.value;
+   
+       if (validateEmail(emValue)) {
+        email.parentElement.classList.remove("error");
+       } else {
+         email.parentElement.classList.add('error');
+       }
+       return false;
+     }
+     btn.addEventListener("click", validate);
+     
+    
+   
+   
+   
 
         
-    });
-    
-    
-}
