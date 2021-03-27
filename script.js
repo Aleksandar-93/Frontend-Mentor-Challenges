539fd4ac2143bee6bb6bd01e163cd2e602e5b360
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-03-27 16:40:36 +0100

    generating buttons based on array

--- script.js
+++ script.js
@@ -1,45 +1,84 @@
 // filter buttons
-const all = document.querySelector('#all');
-const htmlCss = document.querySelector('#html-css');
-const htmlCssJs = document.querySelector('#html-css-js');
 const listItems = document.querySelectorAll('ul li');
+const projects = document.querySelectorAll('#list li');
+const projectType = [];
+const contener = document.querySelector('.buttons');
+
+
+// Buttons
+
+
+projects.forEach(element => {
+    projectType.push(element.getAttribute('class'))
+});
+
+const uniqueV = ['all',...new Set (projectType)];
 
 
 
-// filter buttons
+    // for evry unique value create button
+    uniqueV.forEach(name => {
+        const button = document.createElement('button');
+        button.type= 'button';
+        button.appendChild(document.createTextNode(name));
+        button.setAttribute('id',name);
+        contener.appendChild(button);
+    });
 
-// html css elements
-htmlCss.addEventListener('click', () =>{
-    listItems.forEach(e => {
-        const ifContaine=e.classList.contains('html_css_js');
-        if(ifContaine){
-            e.style.display="none";
-        }else{
-            e.style.display="block";
+    
+    
+
+// add addEventListener for evry button 
+const buttons = document.querySelectorAll('.buttons button')
+
+    buttons.forEach((e) => {
+        
+        // html css elements
+        if(e.textContent === 'html_css'){
+            e.addEventListener('click', () =>{
+                listItems.forEach(e => {
+                    const ifContaine=e.classList.contains('html_css_js');
+                    if(ifContaine){
+                        e.style.display="none";
+                    }else{
+                        e.style.display="block";
+            
+                    }
+                });
+            });
 
         }
-    });
-});
 
-// html css  js elements
-htmlCssJs.addEventListener('click', () =>{
-    listItems.forEach(e => {
-        const ifContaine=e.classList.contains('html_css');
-        if(ifContaine){
-            e.style.display="none";
-        }else{
-            e.style.display="block";
+    // html css  js elements
+    if(e.textContent === 'html_css_js'){
+        e.addEventListener('click', () =>{
+            listItems.forEach(e => {
+                const ifContaine=e.classList.contains('html_css');
+                if(ifContaine){
+                    e.style.display="none";
+                }else{
+                    e.style.display="block";
+        
+                }
+            });
+        });
+    }
 
-        }
-    });
-});
 
-// All elements
-all.addEventListener('click', () =>{
-    listItems.forEach(e => {
-        e.style.display="block";
+    // All elements
+    if(e.textContent === 'all'){
+        e.addEventListener('click', () =>{
+            listItems.forEach(e => {
+                e.style.display="block";
+            });
+        });
+    }
+
+
     });
-});
+
+// buttons end
+
 
  /* \\\\ attribution ////*/
 const amBtn = document.querySelector('.a-m');
@@ -62,3 +101,9 @@ window.addEventListener('scroll', ()=>{
         
     }
 });
+
+
+
+
+
+
